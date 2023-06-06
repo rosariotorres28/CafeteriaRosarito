a8306d6276fb1bb77b5cc09d531ff7ce96132ec1
Rosarito <mariadelrosariotorres21@gmail.com>
2023-06-05 21:57:53 -0300

    modificamos el archivo readme

---
+++ src/components/views/AdministrarProductos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdministrarProductos = () => {
+    return (
+        <div>
+            <h1>Página para administrar productos</h1>
+        </div>
+    );
+};
+
+export default AdministrarProductos;
---
+++ src/components/views/CrearProducto.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const CrearProducto = () => {
+    return (
+        <div>
+            <h1>Página para crear productos</h1>
+        </div>
+    );
+};
+
+export default CrearProducto;
